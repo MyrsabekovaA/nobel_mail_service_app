7d9c960fef8455f3516e1ca263020ed30ea7f386
Dima Pelyp <dpelip03@gmail.com>
2023-12-07 14:30:41 +0100

    setting redux, giving example

--- src/GlobalStates/store.js
+++ src/GlobalStates/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import  isLoggedInReducer  from "/@/GlobalStates/LoggedIn"
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        loggedIn: isLoggedInReducer,
+    },
 })
 
 export default store
