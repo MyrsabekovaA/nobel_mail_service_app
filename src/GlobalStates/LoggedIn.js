bbd91ec497baca7f78d6350dc6ad046931c8138f
Dima Pelyp <dpelip03@gmail.com>
2023-12-10 06:03:33 +0100

    store config example

--- src/GlobalStates/LoggedIn.js
+++ src/GlobalStates/LoggedIn.js
@@ -1,11 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const isLoggedInSlice = createSlice({
-    name: "isLoggedIn",
-    initialState: false,
+    name: "LoggedIn",
+    initialState: {
+      value: false
+    },
     reducers: {
-      changeTheme: (state, action) => {
-        state.value.activeTheme = action.payload;
+      setTrueLogIn: (state, action) => {
+        state.value = true;
+      },
+      setFalseLogIn: (state, action) => {
+        state.value = false
+      },
+      toggleLogIn: (state, action)=> {
+        state.value = !state.value
       }
     }
 });
+
+export const isLoggedInActions = isLoggedInSlice.actions
+export default isLoggedInSlice.reducer
