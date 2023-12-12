a8056cf657024d1af713cfe4a50a78d45e8753b5
Dima Pelyp <dpelip03@gmail.com>
2023-12-12 02:55:11 +0100

    fixing dropdowns

--- src/GlobalStates/store.js
+++ src/GlobalStates/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import  isLoggedInReducer  from "/@/GlobalStates/LoggedIn"
 import sidebarReducer from "/@/GlobalStates/Sidebar"
 import contactsReducer from "/@/GlobalStates/Contacts"
+import languageSlice from "/@/GlobalStates/Language"
 
 const store = configureStore({
     reducer: {
         loggedIn: isLoggedInReducer,
         sidebar: sidebarReducer,
-        contacts: contactsReducer
+        contacts: contactsReducer,
+        language: languageSlice
     },
 })
 
--- src/views/Home/Header/Header.jsx
+++ src/views/Home/Header/Header.jsx
@@ -1,42 +1,34 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import { sidebarActions } from '/@/GlobalStates/Sidebar';
+import { languageActions, LANGUAGE_IMG } from '/@/GlobalStates/Language';
 import English from '/Flag_of_the_U.S..svg';
 import Ukrainian from '/Flag_of_Ukraine.svg';
 import './Header.css';
 import {Icon} from '@iconify/react';
-import {Image, Dropdown} from "react-bootstrap";
+import {Image} from "react-bootstrap";
+import Dropdown from "react-bootstrap/Dropdown"
 
 const Header = (props) => {
     const [darkMode, setDarkMode] = useState(false);
-    const [selectedLanguage, setSelectedLanguage] = useState(English);
     const [showDropdown, setShowDropdown] = useState(false);
-    const dropdownRef = useRef(null);
 
     const dispatch = useDispatch()
 
     const sidebarOpenned = useSelector((state)=>state.sidebar.openned)
     const toggleSidebar = sidebarActions.toggleSidebarOpenned
-    const handleSelectLanguage = (languageFlag) => {
-        setSelectedLanguage(languageFlag);
-        setShowDropdown(false);
-    };
 
-    useEffect(() => {
-        function handleClickOutside(event) {
-            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-                setShowDropdown(false);
-            }
-        }
+    const language = useSelector((state)=>state.language.value)
+    const languageImg = LANGUAGE_IMG[language]
+    const changeLanguage = (languageName)=>{
+        setShowDropdown(false)
+        return dispatch(
+            languageActions.setLanguage({language: languageName})
+            )
+    }
 
-        document.addEventListener('mousedown', handleClickOutside);
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, [dropdownRef]);
-
-    const handleToggleDropdown = (isOpen) => {
-        setShowDropdown(isOpen);
+    const handleToggleDropdown = () => {
+        setShowDropdown(!showDropdown);
     };
 
     const toggleDarkMode = () => {
@@ -46,31 +38,32 @@ const Header = (props) => {
     return (
         <header className={`flex w-full bg-white shadow-md p-1 lg:h-20 h-16 justify-between`}>
             
-                <button onClick={()=>dispatch(toggleSidebar())}
-                        className={`menu-button transition-opacity justify-self-start duration-300 ${sidebarOpenned ? 'opacity-0 pointer-events-none' : 'opacity-100'}`}>
-                    <Icon icon="material-symbols:menu" />
-                </button>
+            <button onClick={()=>dispatch(toggleSidebar())}
+                    className={`menu-button transition-opacity justify-self-start duration-300 ${sidebarOpenned ? 'opacity-0 pointer-events-none' : 'opacity-100'}`}>
+                <Icon icon="material-symbols:menu" />
+            </button>
             <div className="flex items-center space-x-4">
                 <button onClick={toggleDarkMode} className="p-2">
                     <Icon icon={darkMode ? "ph:moon" : "ph:sun"} className="text-lg"/>
                 </button>
-                <Dropdown show={showDropdown} onToggle={handleToggleDropdown} align="end" className="relative">
+                <Dropdown align="end" id="lang_dropdown">
                     <Dropdown.Toggle variant="transparent" className="p-2">
-                        <Image src={selectedLanguage} className="w-6 h-6" alt="Language" />
+                        <Image src={languageImg} className="w-6 h-6" alt="Language" />
                     </Dropdown.Toggle>
 
-                    <Dropdown.Menu className="bg-white text-gray-800 transition-shadow duration-300 ease-in-out rounded-lg shadow-dropdown mt-2 ml-1.5 w-28" style={{ width: '114px', maxHeight: '180px' }}>
+                    <Dropdown.Menu className="bg-white text-gray-800 transition-shadow duration-300 ease-in-out rounded-lg shadow-dropdown w-28 p-2" style={{ width: '114px', maxHeight: '180px' }}>
                         <Dropdown.Item className="outline-none border-0 m-0 rounded-none p-0 flex items-center justify-start relative"
-                                       onClick={() => handleSelectLanguage(English)}>
+                                       onClick={() => changeLanguage("English")} >
                             <Image src={English} className="w-6 h-6 mr-2"/> English
                         </Dropdown.Item>
+
                         <Dropdown.Item className="outline-none border-0 m-0 rounded-none p-0 flex items-center justify-start relative"
-                                       onClick={() => handleSelectLanguage(Ukrainian)}>
+                                       onClick={() => changeLanguage("Ukrainian")}>
                             <Image src={Ukrainian} className="w-6 h-6 mr-2"/> Українська
                         </Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
-                <button onClick={toggleDarkMode} className="p-2">
+                <button className="p-2">
                     <Icon icon="fluent:alert-24-regular" />
                 </button>
                 <Dropdown>
@@ -88,5 +81,4 @@ const Header = (props) => {
         </header>
     );
 };
-
 export default Header;
