5db43d7596171eb64da0ec5427d3c65bb6ab3a7e
Dima Pelyp <dpelip03@gmail.com>
2023-12-18 12:40:49 +0100

    basic template

--- src/loaders/EmailTemplates.jsx
+++ src/loaders/EmailTemplates.jsx
@@ -1,12 +1,14 @@
 import { data } from "autoprefixer"
 
-let domain = "http://localhost:3000"
+let domain = "http://52.59.202.2:3000"
 
 //email templates
 export async function emailTemplatesLoader() {
+    let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ODAyZTMwYzc2ZGVjZWVlMjA4ZTYwNSIsImlhdCI6MTcwMjg5OTI2NSwiZXhwIjoxNzAyOTcxMjY1fQ.qAoB2MseeWBzBBEKjppxzu6ZZpjLE2y4BIREHD3rW-U'
  let templates = await fetch(`${domain}/api/mail-templates`, {
+    method: "GET",
     "headers": {
-        'content-type': "aplication/json"
+        Authorization: `Bearer ${token}`,
     }
  })
  console.log(templates)
--- src/views/Home/EmailTemplates/EmailTemplates.jsx
+++ src/views/Home/EmailTemplates/EmailTemplates.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import EmailTemplate from "./EmailTemplate/EmailTemplate";
+import "./EmailTemplates.css"
 function EmailTemplates(props) {
     let templates = useLoaderData().data
-    return <div>
-        <h2>emailTemplates</h2>
-        <span>{templates.map((template)=>{
-            return <div>{template.id}</div>
-        })}</span>
+    let [allInputsValue, setAllInputsValue] = useState(false)
+    function changeAllInputsValues(e) {
+        setAllInputsValue(e.target.checked)
+    }
+    return <div className="w-full p-4 bg-slate-100 dark:bg-gray-900" >
+        <div className="">
+            <button className="action-btn ">create</button>
+            <button className="action-btn  ml-3">inspect</button>
+            <button className="action-btn  ml-3">edit</button>
+            <button className="action-btn delete-btn  ml-3">delete</button>
+        </div>
+        <table className="mt-3 text-gray-500 dark:text-gray-400">
+            <thead>
+                <tr>
+                    <th className="email-templates__table-cell">
+                        <input type="checkbox" onClick={changeAllInputsValues}/>
+                    </th>
+                    <th className="email-templates__table-cell">Id</th>
+                    <th className="email-templates__table-cell">Name</th>
+                    <th className="email-templates__table-cell">Link</th>
+                </tr> 
+            </thead>
+            <tbody>
+                {templates.map((template)=>{
+                    return <EmailTemplate templateInfo={template} checked={allInputsValue}/>
+                })}
+            </tbody>
+        </table>
     </div>
 }
 
